0d66a0c537e0cc0a0238ed41cc9a335533f6d37a
Michael Breyton <etheanore@gmail.com>
2024-08-20 17:10:28 +0200

    Test tailwind size

--- src/lib/components/experience/Experience.svelte
+++ src/lib/components/experience/Experience.svelte
@@ -4,40 +4,10 @@
 	import Title from './Title.svelte';
 </script>
 
-<header>
-	<div>
+<header class="flex justify-around flex-wrap">
+	<div class="order-1 md:order-2 md:ml-4">
 		<Title />
 		<Text />
 	</div>
 	<Image />
 </header>
-
-<style>
-	header {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		width: 100%;
-		justify-content: space-between;
-	}
-	header > div {
-		order: 0; /* Par défaut, le texte est en deuxième position */
-	}
-	header > :global(img) {
-		order: 0; /* Par défaut, l'image est en première position */
-	}
-
-	@media (min-width: 800px) {
-		header {
-			flex-direction: row;
-			justify-content: space-between;
-			align-items: center;
-		}
-		header > div {
-			order: 1; /* Réinitialiser l'ordre pour les écrans plus larges */
-		}
-		header > div {
-			order: 2; /* Par défaut, le texte est en deuxième position */
-		}
-	}
-</style>
